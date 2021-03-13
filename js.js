2bbe5da8e11e6fc4d8d8f94bc6b557b11266c663
meshall2019 <mmas3l222@gmail.com>
2021-03-13 17:48:39 +0300

    l

--- js.js
+++ js.js
@@ -12,38 +12,40 @@ $("button").click(function(){
 if(name==null){
 
     name=$(".name").val()
-    $(".name").hide()
-    $(".age").show()
+    $(".name").fadeOut(600, function(){$(".age").fadeIn()
+    })
+    
 }
 else if(age==null){
 
     age=$(".age").val()
-    $(".age").hide()
-    $(".work").show()
+    $(".age").fadeOut(600,function(){$(".work").fadeIn()})
+    
 }
 else if(work==null){
 
     work=$(".work").val()
-    $(".work").hide()
-    $(".emil").show()
+    $(".work").fadeOut(600,function(){$(".emil").fadeIn()})
+    
 }
 else if(emil==null){
 
     emil=$(".emil").val()
-    $(".emil").hide()
-    $(".add_img").show()
+    $(".emil").fadeOut(600,function(){$(".add_img").fadeIn()})
+    
 }
 else if(img==null){
 
     img=$(".add_img").val()
-    $(".input-data").hide()
+    $(".input-data").fadeOut(600,function(){
 
     $(".name-show").html(name)
     $(".age-show").html(age)
     $(".work-show").html(work)
     $(".emil-show").html(emil)
     $("img").attr('src',img)
-    $(".card-data").show()
+    $(".card-data").fadeIn(500)
+})
 
 }
 
